f5ca600b8888d66d807796e185852b68b275f6d1
Eduardo Souza <eduardosouzv@gmail.com>
2021-03-02 15:41:40 -0300

    nav items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,40 +1,14 @@
 import Link from 'next/link';
-import { useState } from 'react';
 
 export default function NavBar() {
-  const [homeButton, homeButtonIsActive] = useState(true);
-  const [rankButton, rankButtonIsActive] = useState(false);
-
   return (
     <div className="navContainer">
       <img src="icons/icon.svg" className="icon" />
       <div>
         <Link href="/">
-          {homeButton ? (
-            <img src="icons/home.svg" style={{ opacity: '1' }} />
-          ) : (
-            <img
-              src="icons/home.svg"
-              onClick={() => {
-                rankButtonIsActive(false);
-                homeButtonIsActive(true);
-              }}
-            />
-          )}
+          <img src="icons/home.svg" />
         </Link>
-        <Link href="/rank">
-          {rankButton ? (
-            <img src="icons/rank.svg" style={{ opacity: '1' }} />
-          ) : (
-            <img
-              src="icons/rank.svg"
-              onClick={() => {
-                homeButtonIsActive(false);
-                rankButtonIsActive(true);
-              }}
-            />
-          )}
-        </Link>
+        <img src="icons/moon.svg" style={{ width: '30px' }} />
       </div>
     </div>
   );
